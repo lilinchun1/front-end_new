150a498f2166b83fe18d63b884b21afccfc03c37
llc <yxkj@163.com>
2015-01-04 17:09:54 +0800

    9girds

--- dc_9girds/js/script.js
+++ dc_9girds/js/script.js
@@ -1,61 +1,80 @@
-$(document).ready(function(){
-	/* The following code is executed once the DOM is loaded */
-        $('.sponsorFlip').bind("click",function(){
+$(document).ready(function () {
+    /* The following code is executed once the DOM is loaded */
+    $('.sponsorFlip').bind("click", function () {
+        // $(this) point to the clicked .sponsorFlip element (caching it in elem for speed):
+        var elem = $(this);
+        // data('flipped') is a flag we set when we flip the element:
+        if (elem.data('flipped')) {
+            // If the element has already been flipped, use the revertFlip method
+            // defined by the plug-in to revert to the default state automatically:
+            elem.revertFlip();
+            // Unsetting the flag:
+            elem.data('flipped', false)
+        }
+        else {
+            // Using the flip method defined by the plugin:
+            elem.flip({
+                direction: 'lr',
+                speed: 250,
+                onBefore: function () {
+                    // Insert the contents of the .sponsorData div (hidden from view with display:none)
+                    // into the clicked .sponsorFlip div before the flipping animation starts:
 
-            // $(this) point to the clicked .sponsorFlip element (caching it in elem for speed):
-
-            var elem = $(this);
-
-            // data('flipped') is a flag we set when we flip the element:
-
-            if(elem.data('flipped'))
-            {
-                // If the element has already been flipped, use the revertFlip method
-                // defined by the plug-in to revert to the default state automatically:
-
-                elem.revertFlip();
-
-                // Unsetting the flag:
-                elem.data('flipped',false)
-            }
-            else
-            {
-                // Using the flip method defined by the plugin:
-
-                elem.flip({
-                    direction:'lr',
-                    speed: 250,
-                    onBefore: function(){
-                        // Insert the contents of the .sponsorData div (hidden from view with display:none)
-                        // into the clicked .sponsorFlip div before the flipping animation starts:
-
-                        elem.html(elem.siblings('.sponsorData').html());
-                    }
-                });
-
-                // Setting the flag:
-                elem.data('flipped',true);
-            }
-        });
+                    elem.html(elem.siblings('.sponsorData').html());
+                }
+            });
+            // Setting the flag:
+            elem.data('flipped', true);
+        }
+    });
 
 
-
-    var time2 = 6000;  //重置牌时间
-    var time1 = 10000;
+    /*var time2 = 600;  //重置牌时间
+    var time1 = 1200;
 
     var timer = null;
     var show_c = null;
 
     timer = setInterval(function () {
+        var num2 = parseInt(Math.random() * 9);
+        console.log(num2);
+        $(".sponsorFlip:eq(" + num2 + ")").trigger("click");
+        show_c = setTimeout(function () {
+            $(".sponsorFlip:eq(" + num2 + ")").revertFlip();
+        }, time2);
+    }, time1)*/
 
-        var num2 = parseInt(Math.random() * (9 - 1 + 1) );
-        $(".sponsorFlip:eq("+num2+")").trigger("click");
+    function createRandom2(num , from , to)
+    {
+        var arr=[];
+        var json={};
+        while(arr.length<num)
+        {
+            //产生单个随机数
+            var ranNum=Math.ceil(Math.random()*(to-from))+from;
+            //通过判断json对象的索引值是否存在 来标记 是否重复
+            if(!json[ranNum])
+            {
+                json[ranNum]=1;
+                arr.push(ranNum);
+            }
+        }
+        return arr;
+    }
 
-        show_c =setTimeout(function () {
-
-            $(".sponsorFlip:eq("+num2+")").revertFlip();
-
-        },time2);
-    },time1)
+    function fanpai() {
+       var p = createRandom2(3,0,8);
+        console.log(p);
+        $(".sponsorFlip:eq(" + p[0] + ")").trigger("click");
+        $(".sponsorFlip:eq(" + p[1] + ")").trigger("click");
+        $(".sponsorFlip:eq(" + p[2] + ")").trigger("click");
+        setTimeout(function () {
+            $(".sponsorFlip:eq(" + p[0] + ")").revertFlip();
+            $(".sponsorFlip:eq(" + p[1] + ")").revertFlip();
+            $(".sponsorFlip:eq(" + p[2] + ")").revertFlip();
+        },1000)
+    }
+    setInterval(fanpai,2000);
+    fanpai();
 
 });
